b8f066f70418dc383f33a28d8598bb0adfee10b4
Mohamedwaleeddd <122593986+Mohamedwaleeddd@users.noreply.github.com>
2023-08-29 07:17:16 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,67 @@
+// Menu Box
+function show(){
+  document.querySelector('.menu').style.top = "0";
+}
+function d(){
+  document.querySelector('.menu').style.top = "-100%";
+}
+
+//Slider
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) { slideIndex = 1 }
+  if (n < 1) { slideIndex = slides.length }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " active";
+}
+
+//Arrow Up Button
+const arrowUp = document.getElementById("arrow-up");
+
+window.addEventListener("scroll", () => {
+  if (document.body.scrollTop >= 100 || document.documentElement.scrollTop >= 100) {
+    arrowUp.style.display = "block";
+  } else {
+    arrowUp.style.display = "none";
+  }
+});
+arrowUp.addEventListener("click", () => {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+});
+
+//Animations
+
+window.addEventListener("scroll", () => {
+  if (document.body.scrollTop >= 100 || document.documentElement.scrollTop >= 400) {
+  document.querySelector('.about').style.left = "0";
+  } else {
+  document.querySelector('.about').style.left = "-100%";
+  }
+});
+window.addEventListener("scroll", () => {
+  if (document.body.scrollTop >= 100 || document.documentElement.scrollTop >= 1320) {
+    document.querySelector('.skills-cont').style.opacity = "1";
+  }else{
+   document.querySelector('.skills-cont').style.opacity = "0";
+  }
+});
